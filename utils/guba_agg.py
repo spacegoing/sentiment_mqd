d91aeb53ca0cbffab59bd1a677303e51fa091dad
spacegoing <spacegoing@gmail.com>
2018-10-17 16:55:11 +1100

    [New] unique stockcodes histogram

--- utils/guba_agg.py
+++ utils/guba_agg.py
@@ -2,6 +2,7 @@
 import pickle
 from pymongo import MongoClient
 from collections import defaultdict
+from functools import partial
 import pandas as pd
 
 client = MongoClient('mongodb://localhost:27017/')
@@ -31,14 +32,17 @@ def get_date_stock_dict(all_cols):
 
 
 def get_date_time_hist(all_cols):
-  date_dict = defaultdict(dict)
+  '''
+  in 30 mins interval, how may seconds has comment
+  '''
+  date_dict = defaultdict(partial(defaultdict, int))
   for i, col in enumerate(all_cols):
     for doc in col.find():
       date_dict[doc['post_datetime'].date()][doc['post_datetime'].time()
-                                             .strftime("%H:%M:%S")] = 1
+                                             .strftime("%H:%M:%S")] += 1
       for c in doc['comment_dict_list']:
         date_dict[c['comment_time'].date()][c['comment_time'].time().strftime(
-            "%H:%M:%S")] = 1
+            "%H:%M:%S")] += 1
 
   date_df = pd.DataFrame(date_dict)
   date_df.index = pd.to_datetime(date_df.index)
@@ -53,11 +57,49 @@ def get_date_time_hist(all_cols):
   hist['sum'] = hist.sum(axis=1)
   return hist
 
+
 SH_hist = get_date_time_hist(SH_cols)
 SZ_hist = get_date_time_hist(SZ_cols)
 SH_hist.to_csv('SH_hist.csv')
 SZ_hist.to_csv('SZ_hist.csv')
 
+
+def get_ustocks_date_time_hist(all_cols):
+  '''
+  how many unique stocks has comments in each 30 mins
+  '''
+  date_time_stock_dict = defaultdict(partial(defaultdict, dict))
+  for i, col in enumerate(all_cols):
+    for doc in col.find():
+      date_time_stock_dict[doc['post_datetime'].date()][doc[
+          'post_datetime'].time().strftime("%H:%M:%S")][col.name] = 1
+      for c in doc['comment_dict_list']:
+        date_time_stock_dict[c['comment_time'].date()][c[
+            'comment_time'].time().strftime("%H:%M:%S")][col.name] = 1
+
+  date_dict = defaultdict(dict)
+  for date, time_stock_dict in date_time_stock_dict.items():
+    for time, stock_dict in time_stock_dict.items():
+      date_dict[date][time] = len(stock_dict.keys())
+
+  date_df = pd.DataFrame(date_dict)
+  date_df.index = pd.to_datetime(date_df.index)
+  date_list = pd.date_range('09/01/2018', '09/30/2018')
+  date_list = [i.date() for i in date_list]
+  date_df = date_df.transpose()
+  date_df.sort_index(inplace=True)
+  date_df = date_df.loc[date_list[0]:date_list[-1]]
+  date_df = date_df.transpose()
+  date_df.sort_index(inplace=True)
+  hist = date_df.groupby(pd.Grouper(freq='30Min')).sum()
+  hist['sum'] = hist.sum(axis=1)
+  return hist
+
+
+SH_ustocks_hist = get_ustocks_date_time_hist(SH_cols)
+SZ_ustocks_hist = get_ustocks_date_time_hist(SZ_cols)
+SH_ustocks_hist.to_csv('SH_ustocks_hist.csv')
+SZ_ustocks_hist.to_csv('SZ_ustocks_hist.csv')
 
 with open('SH_date_stock_dict.pickle', 'wb') as f:
   pickle.dump(get_date_stock_dict(SH_cols), f)
